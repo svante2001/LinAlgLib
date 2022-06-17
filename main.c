a214f97fd0a7a0ff42d51973829cedf9e974196d
svante2001 <svante2001@gmail.com>
2022-06-17 15:32:48 +0200

    Matrix product example

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include "Core/Matrix.h"
+#include "MatrixOperations/MatrixArithmetics.h"
 
 int main() {
-    Matrix* m = CreateMatrix(5, 5);
-    SetEntry(m, 2, 2, 3.14);
-    PrintMatrix(m);
-    printf("\nEntry 2x2: %f\n", GetEntry(m, 2, 2));
+    double a[] = { 1.0, 2.0, 3.0, 
+                   4.0, 5.0, 6.0 };
+    Matrix* m1 = SetMatrix(a, 2, 3);
+
+    double b[] = { 7.0, 8.0, 
+                   9.0, 10.0,
+                   11.0, 12.0};
+    Matrix* m2 = SetMatrix(b, 3, 2);
+    
+    PrintMatrix(m1);
+    PrintMatrix(m2);
+    PrintMatrix(MatrixProduct(m1, m2));
 }
