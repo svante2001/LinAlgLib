a635ef026a1e550fb7d88c19dd885257910d9898
svante2001 <svante2001@gmail.com>
2022-06-17 14:00:21 +0200

    Testing methods

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include "Core/Matrix.h"
 
 int main() {
-    Matrix* m = CreateMatrix(5,5);
+    Matrix* m = CreateMatrix(5, 5);
+    SetEntry(m, 2, 2, 3.14);
     PrintMatrix(m);
+    printf("\nEntry 2x2: %f\n", GetEntry(m, 2, 2));
 }
