47c7476ce1bf28f0a13eb729df9ea9be24d91cec
svante2001 <svante2001@gmail.com>
2022-06-18 18:02:32 +0200

    Transpose example

---
+++ Examples/TransposeMatrixExample.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "./../Core/Matrix.h"
+#include "./../MatrixOperations/MatrixArithmetics.h"
+
+int main() {
+    double a[] = { 1.0, 2.0, 3.0, 
+                   4.0, 5.0, 6.0 };
+    Matrix* m = SetMatrix(a, 2, 3);
+    
+    PrintMatrix(m);
+    PrintMatrix(TransposeMatrix(m));
+}
