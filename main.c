2eb6375fbfcd4558c0e27da6abeb9581e0f256d4
svante2001 <svante2001@gmail.com>
2022-07-02 21:20:55 +0200

    Freeing the example matrix

--- main.c
+++ main.c
@@ -4,9 +4,11 @@
 
 int main() {
     double a[] = { 1.0, 2.0, 3.0, 
-                   4.0, 5.0, 6.0 };
-    Matrix* m = SetMatrix(a, 2, 3);
+                   4.0, 5.0, 6.0,
+                   7.0, 8.0, 9.0 };
+    Matrix* m = SetMatrix(a, 3, 3);
     PrintMatrix(m);
+    FreeMatrix(m);
 
     // See README.md for all the implemented functions.
 }
