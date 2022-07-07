6783ed2dd5bc4fb2d66ed8eacdd2c78b17401154
svante2001 <svante2001@gmail.com>
2022-07-07 18:16:19 +0200

    Getting / setting vectors

--- Core/Vector.c
+++ Core/Vector.c
@@ -20,6 +20,26 @@ void FreeVector(Vector* v) {
     free((Vector*)v);
 }
 
+int Height(Vector* v) {
+    return v->height;
+}
+
+double GetElement(Vector* v, int index) {
+    return v->element[index-1];
+}
+
+void SetElement(Vector* v, int index, double value) {
+    v->element[index-1] = value;
+}
+
+Vector* SetVector(double* arr, int height) {
+    Vector* v = CreateVector(height);
+    for (int i = 1; i <= height; i++) {
+        SetElement(v, i, arr[i-1]);
+    }
+    return v;
+}
+
 void PrintVector(Vector* v) {
     printf("\n");
     for (int i = 0; i < v->height; i++) {
--- Core/Vector.h
+++ Core/Vector.h
@@ -5,6 +5,10 @@ typedef struct Vector Vector;
 
 Vector* CreateVector(int h);
 void FreeVector(Vector* v);
+int Height(Vector* v);
+double GetElement(Vector* v, int index);
+void SetElement(Vector* v, int index, double value);
+Vector* SetVector(double* arr, int height);
 void PrintVector(Vector* v);
 
 #endif
